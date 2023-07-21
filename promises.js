8b0b90db51b8f127ee7c86cc51d80ecbe62567c8
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-07-21 12:51:34 +0200

    Promises

--- promises.js
+++ promises.js
@@ -14,14 +14,25 @@ function getPosts(){
             output += `<li>${post.title}</li>`;
         });
         document.body.innerHTML = output
-    }, 5000 /*setTimeou() ==> the amount of time it takes*/);
+    }, 2000 /*setTimeout() ==> the amount of time it takes*/);
 }
 
-function createPost(post, callback){
-    setTimeout(function(){
-        posts.push(post);
-        callback();
-    }, 50)
+function createPost(post){
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            posts.push(post);
+
+            const error = true;
+
+            if(!error){
+                resolve();
+            }else {
+                reject('Error, something went wrong')
+            }
+        }, 2000)
+    });
 }
 
-createPost({ title:'Post six', body: 'this is post six'}, getPosts);
+createPost({ title:'Post six', body: 'this is post six'})
+    .then(getPosts)
+    .catch(err => console.log(err));
